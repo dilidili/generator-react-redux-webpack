dfe9c9f7edf1a4f095908c5e4d9b422e2b07e975
XuMM_12 <macbook@XuMM-12.local>
2016-07-17 17:54:22 +0800

    [add] Tweet detail page static style

--- src/containers/homeindex/TweetDetail.jsx
+++ src/containers/homeindex/TweetDetail.jsx
@@ -8,6 +8,29 @@ const TweetDetail = React.createClass({
 		tid: PropTypes.string,
 	},
 
+
+	// Renders
+	renderContent(){
+		return (
+			<div className={styles.mainContent}>
+				<div className={styles.retweetLabel}><span className="Icon Icon--retweet"></span> Dan Abramov 转发了此微博</div>
+				<div className={styles.avatarGroup}>
+					<img src="https://pbs.twimg.com/profile_images/729685404568330240/5y91rCy2_bigger.jpg"/>					
+					<p>Micheal Chan</p>
+					<button><span className="Icon Icon--follow"></span></button>
+				</div>
+				<div className={styles.textGroup}>
+					i hear all the time companies interview for "culture fit". i think that's part of the problem. maybe you should hire for "culture shakeup".
+				</div>
+				<div className={styles.timestamp}>7/13/16, 05:34</div>
+				<div className={styles.status}>
+					<p><span className={styles.count}>5</span> 转发</p>
+					<p><span className={styles.count}>9</span> 赞</p>
+				</div>
+			</div>
+		)
+	},
+
 	render: function(){
 		const {
 			tid,
@@ -18,6 +41,7 @@ const TweetDetail = React.createClass({
 
 		return (
 			<div className={styles.container}>
+				{this.renderContent()}		
 			</div>
 		)
 	},
