959d4459e44eebd9849f7be94f01b17d73a0171a
WenSheng Xu <wensheng@ngs.tech>
2016-07-20 15:00:17 +0800

    [add] render static spinner petal

--- src/enhancers/PullToRefreshEnhancer.jsx
+++ src/enhancers/PullToRefreshEnhancer.jsx
@@ -5,7 +5,8 @@ import styles from './PullToRefreshEnhancer.scss'
 import _ from 'underscore'
 import {Motion, spring} from 'react-motion'
 
-const SPINNEER_CONTAINER_HEIGHT = 3 * window.fontSize
+const SPINNER_CONTAINER_HEIGHT = 3 * window.fontSize
+const SPINNER_RADIUS = SPINNER_CONTAINER_HEIGHT / 4
 
 /**
  * @param {Component} DataReceiverComponent
@@ -18,10 +19,54 @@ export default function PullToRefreshEnhancer(DataReceiverComponent, ConcreteCom
 				pullDistance: 0, // The length of the ConcreteCompoment moves down, 
 			}
 		},
-		componentWillMount(){
-			// precompute spinner petal layout datas
-			this.PETAL_DELTA = SPINNEER_CONTAINER_HEIGHT * Math.PI / 12 // spinner consists of 12 petals
-			this.PETAL_COUNT = window.innerWidth / this.PETAL_DELTA + 12
+		componentWillMount() {
+			// Precompute spinner petal layout datas
+			this.PETAL_DELTA = SPINNER_CONTAINER_HEIGHT * Math.PI / 12 // spinner consists of 12 petals
+			this.PARABOLIC_PETAL_COUNT = Math.round(window.innerWidth / (2 * this.PETAL_DELTA))
+			this.PETAL_COUNT = this.PARABOLIC_PETAL_COUNT * 2 + 12
+
+			// Calculate position and threshoud for every spinner petal
+			const k = SPINNER_CONTAINER_HEIGHT / Math.pow(window.innerWidth, 2)
+			const k_z = 360 / Math.pow(window.innerWidth, 2)
+			this.petalMeta = _.map(_.range(this.PETAL_COUNT), index => {
+				// calculate position
+				let position
+				let rotateZ = 90
+				if (index < this.PARABOLIC_PETAL_COUNT) {
+					// petals march
+					const x = index * this.PETAL_DELTA
+					const y = k * x * x
+					position = [x, y]
+					rotateZ = - 90 + k_z * x * x
+					
+				// one spinner contains 12 petals
+				} else if (index < this.PARABOLIC_PETAL_COUNT + 12) {
+					// petals on spinner
+					index = index - this.PARABOLIC_PETAL_COUNT
+					const radians = index * Math.PI / 6
+					position = [
+						window.innerWidth / 2 + Math.sin(radians) * SPINNER_RADIUS,
+						SPINNER_CONTAINER_HEIGHT / 4 + (1 - Math.cos(radians)) * SPINNER_RADIUS,
+					]
+					rotateZ = index * 30
+				} else {
+					// petals leave
+					index = 2 * this.PARABOLIC_PETAL_COUNT + 12 - index - 1
+					const x = index * this.PETAL_DELTA
+					const y = k * x * x
+					position = [window.innerWidth - x, y]
+					rotateZ = 90 - k_z * x * x
+				}
+
+				// calculate threshold
+				const threshold = index * SPINNER_CONTAINER_HEIGHT / this.PETAL_COUNT
+
+				return {
+					position,
+					rotateZ,
+					threshold,
+				}
+			})
 		},
 
 		// Handler
@@ -38,19 +83,34 @@ export default function PullToRefreshEnhancer(DataReceiverComponent, ConcreteCom
 		},
 		handleTouchEnd(evt){
 			this.setState({
-				pullDistance: 30,
+				pullDistance: SPINNER_CONTAINER_HEIGHT,
 			})
 		},
 
 		// Render
 		renderSpinner(deltaHeight){
-			<div className={styles.spinnerContainer}
+			return <div className={styles.spinnerContainer}
 				style={{
 					top: -deltaHeight,	
 					height: deltaHeight,
 					backgroundColor: '#f9fafd',
 				}}
 			>
+				{
+					_.map(this.petalMeta, (v,k)=>
+						<div
+							className={styles.spinnerPetal}
+							key={k}
+							style={{
+								left: v.position[0],
+								top: v.position[1],
+								transform: `rotateZ(${v.rotateZ}deg)`,
+								WebkitTransform: `rotateZ(${v.rotateZ}deg)`,
+							}}
+						>
+						</div>
+					)	
+				}
 			</div>
 		},
 
